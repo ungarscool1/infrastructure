f513e43e15f86fe147853abd93970e76b6e4935f
Léo Godard <leo@legodard.fr>
2024-06-30 02:37:50 +0200

    replace cf-ingress by traefik

--- stack/vaultwarden/secret.tf
+++ stack/vaultwarden/secret.tf
@@ -1,26 +1,12 @@
 resource "random_password" "vaultwarden_db_password" {
   length           = 32
-  special          = true
-  override_special = "_%@"
-}
-
-resource "random_string" "vaultwarden_db_name" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  special          = false
 }
 
 resource "hcp_vault_secrets_app" "vaultwarden" {
   app_name    = "vaultwarden"
   project_id = data.terraform_remote_state.vault.outputs.project_id
   description = "Vaultwarden application secrets"
-}
-
-resource "hcp_vault_secrets_secret" "db_name" {
-  app_name     = "vaultwarden"
-  project_id = data.terraform_remote_state.vault.outputs.project_id
-  secret_name  = "POSTGRES_DB"
-  secret_value = random_string.vaultwarden_db_name.result
 }
 
 resource "hcp_vault_secrets_secret" "db_password" {
