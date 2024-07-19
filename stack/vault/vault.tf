05f974a29bcd246f5217c3145b704a2e3b0ece23
Léo Godard <leo@legodard.fr>
2024-07-19 20:08:49 +0200

    add sec and hcp creds in vault ns

--- stack/vault/vault.tf
+++ stack/vault/vault.tf
@@ -11,13 +11,12 @@ resource "kubectl_manifest" "secret" {
   depends_on = [
     helm_release.vault_agent,
     hcp_service_principal_key.self,
-    helm_release.cluster_secret
+    helm_release.cluster_secret,
+    kubernetes_namespace.vault
   ]
   yaml_body = templatefile("./manifests/vault-auth-secret.yaml", {
     globalClientId: base64encode(hcp_service_principal_key.self.client_id),
-    globalClientSecret: base64encode(hcp_service_principal_key.self.client_secret),
-    vaultClientId: base64encode(hcp_service_principal_key.vault.client_id),
-    vaultClientSecret: base64encode(hcp_service_principal_key.vault.client_secret),
+    globalClientSecret: base64encode(hcp_service_principal_key.self.client_secret)
   })
   sensitive_fields = ["data"]
 }
@@ -27,8 +26,11 @@ resource "kubectl_manifest" "authentication" {
     kubectl_manifest.secret
   ]
   yaml_body = templatefile("./manifests/vault-auth.yaml", {
+    name: "default",
+    namespace: "vault-secrets-operator-system",
     organizationId : data.hcp_organization.organization.resource_id,
     projectId : hcp_project.self.resource_id,
+    secretRef: "vault-auth"
   })
 }
 
@@ -42,4 +44,31 @@ resource "kubernetes_namespace" "vault" {
   metadata {
     name = "vault"
   }
+  lifecycle {
+    ignore_changes = [ metadata[0].annotations ]
+  }
+}
+
+resource "kubernetes_secret" "vault" {
+  metadata {
+    name = "hcp-vault-auth"
+    namespace = kubernetes_namespace.vault.metadata[0].name
+  }
+  data = {
+    "clientID" = base64encode(hcp_service_principal_key.vault.client_id)
+    "clientSecret" = base64encode(hcp_service_principal_key.vault.client_secret)
+  }
+}
+
+resource "kubectl_manifest" "hcp_vault_auth" {
+  depends_on = [
+    kubectl_manifest.secret
+  ]
+  yaml_body = templatefile("./manifests/vault-auth.yaml", {
+    name: kubernetes_secret.vault.metadata[0].name,
+    namespace: kubernetes_namespace.vault.metadata[0].name,
+    organizationId : data.hcp_organization.organization.resource_id,
+    projectId : hcp_project.self.resource_id,
+    secretRef: kubernetes_secret.vault.metadata[0].name
+  })
 }
